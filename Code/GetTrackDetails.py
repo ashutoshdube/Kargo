321644c70fdb5d4b71bb719b072c2be9224c8f80
Ashutosh <aashutosh.dube@gmail.com>
2017-01-12 01:19:25 -0800

    adding code and image

---
+++ Code/GetTrackDetails.py
@@ -0,0 +1,57 @@
+import h5py
+import os
+import sys
+from pyspark import SparkContext
+
+
+## Get all files
+root = "/Users/Ashutosh/Documents/Kargo/MillionSongSubset/data"
+
+output = []
+
+for path, subdirs, files in os.walk(root):
+    for name in files:
+    	if(name.lower().endswith(".h5")):
+            fileObj = h5py.File(os.path.join(path, name))
+
+            fileDict = {}
+            
+            fileDict["artist_name"] = fileObj["metadata"]["songs"][0][9]
+        	
+            fileDict["release"] = fileObj["metadata"]["songs"][0][14]
+        	
+            fileDict["title"] = fileObj["metadata"]["songs"][0][18]
+
+            fileDict["genre"] = fileObj["metadata"]["songs"][0][11]
+        	
+            fileDict["year"] = fileObj["musicbrainz"]["songs"][0][1]
+
+            fileDict["duration"] = fileObj["analysis"]["songs"][0][3]
+        	
+            fileDict["track_id"] = fileObj["analysis"]["songs"][0][30]
+
+            fileDict["loudness"] = fileObj["analysis"]["songs"]["loudness"][0]
+
+            fileDict["tempo"] = fileObj["analysis"]["songs"]["tempo"][0]
+
+            fileDict["energy"] = fileObj["analysis"]["songs"]["energy"][0]
+
+            fileDict["danceability"] = fileObj["analysis"]["songs"]["danceability"][0]
+
+            fileDict["artist_hotttnesss"] = float(fileObj["metadata"]["songs"]["artist_hotttnesss"][0])
+
+            fileDict["artist_familiarity"] = float(fileObj["metadata"]["songs"]["artist_familiarity"][0])
+
+            fileDict["artist_location"] = fileObj["metadata"]["songs"]["artist_location"][0]
+
+            output.append(fileDict)
+
+
+#print output
+print "Count of Records : " + str(len(output))
+
+sc = SparkContext("local", "Simple App")
+
+fieldRdd = sc.parallelize(output)
+
+
